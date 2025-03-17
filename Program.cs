6fa98a2dd30a6084905c769eb4d48b4e9e640667
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-17 19:55:34 +0100

    Final Commit
    completed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(2, 8);
+            Rectangle r2 = new Rectangle(4, 5);
+            Rectangle r3 = r1 + r2;
+            Console.WriteLine(r1.getRectangle());
+            Console.WriteLine(r2.getRectangle());
+            Console.WriteLine(r3.getRectangle());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Generator
+{
+    class Rectangle
+    {
+
+        //Attributes
+        double length, breadth;
+
+        //Methods
+        public 
+            Rectangle()
+        {
+            this.length = 0;
+            this.breadth = 0;
+        }
+        public Rectangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public string getRectangle()
+        {
+            return string.Format("Rectangle : {0}' {1} \"", length, breadth);
+        }
+
+        //Operator overloading
+        public static Rectangle operator +(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r3 = new Rectangle();
+            r3.length = r1.length + r2.length;
+            r3.breadth = r1.breadth + r2.breadth;
+            return r3;
+        }
+    }
+}
